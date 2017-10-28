e9f89a01c9019ded2f27f21375eb68920e202757
abgill <gill.andrew.95@gmail.com>
2017-10-28 06:39:31 +0000

    basic post working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var board = [];
+
+var currentPlayer = 1;
+
+for(var i = 0; i< 7; i++){
+    board.push([]);
+    for(var j = 0; j < 6; j++){
+        board[i][j] = 0;
+    }
+}
+
+router.get('/poll', function(req, res, next) {
+  res.status(200).json(board);
 });
+
+router.post('/turn',function (req, res) {
+    console.log('in turn route');
+    console.log(req.body);
+    res.status(200).json('{data: 3}');
+})
 
 module.exports = router;
